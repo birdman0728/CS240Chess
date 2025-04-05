ed0ba6ae9ff4176e8a5d1bdd86ec36cdd9bb4ec8
Braden Porter <bradenmporter@gmail.com>
2025-04-04 23:50:09 -0600

    Mostly working on Join

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -5,11 +5,17 @@ package client;
 
 import Server.ServerFacade;
 import chess.ChessGame;
+import chess.ChessPiece;
+import chess.ChessPosition;
 import model.AuthData;
+import model.GameData;
 import requestsandresults.*;
+import ui.EscapeSequences;
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Client {
     private final ServerFacade server;
@@ -73,7 +79,7 @@ public class Client {
                 case "create" -> create(params);
                 case "list" -> list();
                 case "play" -> play(params);
-                case "observe" -> observe(params);
+//                case "observe" -> observe(params);
                 case "logout" -> logout();
                 case "quit" -> "quit";
                 default -> help();
@@ -83,33 +89,134 @@ public class Client {
         }
     }
 
-    private String create(String[] params) throws Exception{}
-    private String list() throws Exception{}
+
+
+//    private String observe(String[] params) throws Exception{
+//        if(signedIn){
+//            //TODO figure out observe
+//        }
+//    }
+    private String create(String[] params) throws Exception{ //TODO res is null?
+        if(signedIn && params.length == 1){
+            CreateResult res = server.create(new CreateRequest(AuthToken.authToken(), params[0]));
+            return "Game " + params[0] + " created with id: " + res.gameID() + "\n";
+        }else{
+            throw new Exception("Not signed in or no name added");
+        }
+    }
+
+    private String list() throws Exception{
+        if(signedIn){
+            ListResult res = server.listGames(new ListRequest(AuthToken.authToken()));
+            if(res != null) {
+                String printList = "";
+                for(GameData game : res.games()){
+                    printList += game.gameID() + " " + game.gameName() + " " + "White: " + game.whiteUsername() + " Black: " + game.blackUsername() + "\n";
+                }
+
+                return printList;
+            }else{
+                return "No games exist\n";
+            }
+        }else{
+            throw new Exception("Not logged in\n");
+        }
+    }
 
     private String play(String[] params) throws Exception{
         if(signedIn){
             ChessGame.TeamColor color;
-            if(params[0] == "White"){
+            if(Objects.equals(params[0], "White")){
                 color = ChessGame.TeamColor.WHITE;
-            }else if(params[0] == "Black"){
+            }else if(Objects.equals(params[0], "Black")){
                 color = ChessGame.TeamColor.BLACK;
             }else{
                 throw new Exception("Please specify which team you'd like to join");
             }
+
             int gameID = Integer.parseInt(params[1]);
+            StringBuilder output = new StringBuilder();
+            ChessGame curGame = null;
+
 
             server.join(new JoinRequest(AuthToken.authToken(), color, gameID, AuthToken.username()));
-            return "Joinging Game" + gameID;
+            for(GameData game : server.listGames(new ListRequest(AuthToken.authToken())).games()){
+                if(game.gameID() == gameID){
+                    curGame = game.game();
+                    output.append("Joinging Game: ").append(game.gameName()).append("\n\n");
+                }
+            }
+
+
+            for(int i = 1; i < 9; i++){
+                for(int j = 1; j < 9; j++ ){
+                    assert curGame != null;
+                    ChessPiece.PieceType type = curGame.getBoard().getPiece(new ChessPosition(j,i)).getPieceType();
+                    output.append(CalcPiece(type, color));
+                }
+            }
+
+            return output.toString();
+
         }else{
             throw new Exception("Please specify which game you'd like to join");
         }
     }
 
-    private String observe(String[] params) throws Exception{
-        if(signedIn){
-            //TODO figure out observe
+    private String CalcPiece(ChessPiece.PieceType type, ChessGame.TeamColor color){
+        switch(type){
+            case KING:
+                if(color == ChessGame.TeamColor.BLACK){
+                    return EscapeSequences.BLACK_KING;
+                }else{
+                    return EscapeSequences.WHITE_KING;
+                }
+                break;
+
+            case QUEEN:
+                if(color == ChessGame.TeamColor.BLACK){
+                    return EscapeSequences.BLACK_QUEEN;
+                }else{
+                    return EscapeSequences.WHITE_QUEEN;
+                }
+                break;
+
+            case BISHOP:
+                if(color == ChessGame.TeamColor.BLACK){
+                    return EscapeSequences.BLACK_BISHOP;
+                }else{
+                    return EscapeSequences.WHITE_BISHOP;
+                }
+                break;
+
+            case ROOK:
+                if(color == ChessGame.TeamColor.BLACK){
+                    return EscapeSequences.BLACK_ROOK;
+                }else{
+                    return EscapeSequences.WHITE_ROOK;
+                }
+                break;
+
+            case KNIGHT:
+                if(color == ChessGame.TeamColor.BLACK){
+                    return EscapeSequences.BLACK_KNIGHT;
+                }else{
+                    return EscapeSequences.WHITE_KNIGHT;
+                }
+                break;
+
+            case PAWN:
+                if(color == ChessGame.TeamColor.BLACK){
+                    return EscapeSequences.BLACK_PAWN;
+                }else{
+                    return EscapeSequences.WHITE_PAWN;
+                }
+                break;
+
+            default:
+                return EscapeSequences.EMPTY;
+
         }
-    }
 
     private String logout() throws Exception{
         if(signedIn){
@@ -125,7 +232,7 @@ public class Client {
     private String register(String[] params) throws Exception {
         if(params.length == 3 && !signedIn){
             RegisterResult req = server.register(new RegisterRequest(params[0], params[1], params[2]));
-            AuthToken = new AuthData(req.username(), req.authToken());
+            AuthToken = new AuthData(req.authToken(), req.username());
             signedIn = true;
             return "Successfully registered. \n";
         }else{
@@ -136,7 +243,7 @@ public class Client {
     private String login(String[] params) throws Exception {
         if(params.length == 2 && !signedIn){
             LoginResult req = server.login(new LoginRequest(params[0], params[1]));
-            AuthToken = new AuthData(req.username(), req.authToken());
+            AuthToken = new AuthData(req.authToken(), req.username());
             signedIn = true;
             return "Successfully signed in. \n";
         }else{
